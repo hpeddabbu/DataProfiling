666ffa0c01d37b945fa1278120b5a8e0a6c913f7
Harini <harini5693@gmail.com>
2018-02-06 19:02:05 -0500

    counting no of records in each table

---
+++ Dp.py
@@ -0,0 +1,12 @@
+
+dftables = sqlContext.tables('default')
+#print dftables.show()
+print "Database  Tables and Row Counts"
+df1 = sqlContext.sql("Use default")
+for t in dftables.collect():
+    #print t.tableName
+    query = "select " + '"' +t.tableName + '"' + " as tableName, count(*) as num_rows from " + t.tableName
+    #print query
+    df2 = sqlContext.sql(query)
+    for r in df2.collect():
+        print r.tableName + '|' , r.num_rows
